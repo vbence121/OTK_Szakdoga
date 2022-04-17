3929a685b50f0b158440ab2bbc931f16861566bf
Mate Paluska <paluska.mate@gmail.com>
2022-04-17 21:21:26 +0200

    echo remove

--- PHP_API/index.php
+++ PHP_API/index.php
@@ -5,8 +5,7 @@ require __DIR__ . "\includes\initialize.php";
  
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
-echo($uri);
-exit();
+
 if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
     header("HTTP/1.1 404 Not Found");
     exit();
