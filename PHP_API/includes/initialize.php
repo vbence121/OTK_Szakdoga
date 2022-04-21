bb885fb20278fa1665a12b2c8e73ad2437269de5
vbence121 <vbence121@gmail.com>
2022-04-21 07:19:11 +0200

    Update initialize.php

--- PHP_API/includes/initialize.php
+++ PHP_API/includes/initialize.php
@@ -1,7 +1,6 @@
 <?php
-
     defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-    defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'wamp' .DS.'www'.DS.'szakdoga\PHP_API');
+    defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'wamp' .DS.'www'.DS.'szakdoga');
     defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'includes');
     defined('API_PATH') ? null : define('API_PATH', SITE_ROOT.DS.'API');
     
