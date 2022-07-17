3124ad59e77634e70f635c09897968b91720b0f4
Mate Paluska <paluska.mate@gmail.com>
2022-07-17 20:58:17 +0200

    jest config update

--- otk-frontend/jest.config.js
+++ otk-frontend/jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  verbose: true,
+  moduleFileExtensions: ["js","ts", "json", "vue"],
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+.vue$': '@vue/vue3-jest',
+  },
+  //transformIgnorePatterns: [`/node_modules/(?!${esModules})/(.*)`],
+  transformIgnorePatterns: [`/node_modules/node_modules/vue-spinner/src/ClipLoader.vue`],
+  allowJs: true 
 }
--- otk-frontend/tests/unit/views/registerView.spec.ts
+++ otk-frontend/tests/unit/views/registerView.spec.ts
@@ -1,17 +1,45 @@
 import { mount } from '@vue/test-utils';
 import RegisterView from '@/views/RegisterView.vue';
 import { REGISTER } from '@/labels/labels';
+import ClipLoader from "vue-spinner/src/ClipLoader.vue";
+import axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('registerView Component tests.', () => {
   let wrapper: any;
 
   beforeEach(() => {
-    wrapper = mount(RegisterView);
+    wrapper = mount(RegisterView, {
+      components:{
+        ClipLoader
+      }
+    });
   })
 
   test('Test for labels.', () => {
     const expectedLabels = REGISTER;
     
     expect((wrapper.vm as any).registerLabels).toBe(expectedLabels);
+  });
+
+  test('Test for submitting the form.', () => {
+    mockedAxios.post.mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          name: 'Joe Doe'
+        },
+        {
+          id: 2,
+          name: 'Jane Doe'
+        }
+      ],
+    });
+
+    (wrapper.vm as any).submit()
+    
+    expect(1).toBe(1);
   });
 });
